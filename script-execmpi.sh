60775217d0cc9779b9cc7fc7e29a4111e99b4018
Thais Camacho <thaiscamachoo@gmail.com>
2021-04-10 12:56:42 -0300

    up
    Former-commit-id: 79758cf66181c1783519e3020785d0de6edb9049

--- script-execmpi.sh
+++ script-execmpi.sh
@@ -1,2 +1,15 @@
+DATASET=exp-results
+rm -rf $DATASET
+mkdir $DATASET
+
 cd build
-mpirun -n 2 ./lmp -var t 300 -echo screen -in lj/in.lj
+sudo perf stat -o ../$DATASET/perf-app-lammps.out ./lmp -var t 300 -echo screen -in lj/in.lj
+/usr/bin/time -o ../$DATASET/time-app-lammps.out -v ./lmp -var t 300 -echo screen -in lj/in.lj
+ltrace -o ../$DATASET/mpi-app-lammps.out ./lmp -var t 300 -echo screen -in lj/in.lj
+
+cd ../$DATASET
+cat mpi-app-lammps.out | grep "MPI" > mpi2-app-lammps.out
+rm mpi-app-lammps.out
+mv mpi2-app-lammps.out mpi-app-lammps.out
+
+#mpirun -n 2 ./lmp -var t 300 -echo screen -in lj/in.lj
