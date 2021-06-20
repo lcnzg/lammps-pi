1da08d83188055d972dcedeacfdfa7de3603736c
Thais <thaiscamachoo@gmail.com>
2021-06-19 21:12:19 -0300

    up
    Former-commit-id: 9a81126c3cdfc399482c67d848c87818047f690b

--- exec_scripts/run_exec.sh
+++ exec_scripts/run_exec.sh
@@ -4,12 +4,10 @@ HOSTFILE=$PWD/hostfile
 DATASET=$PWD/$1
 mkdir $DATASET
 cd ..
-lammps_a_app="./../lmp -in in.lj"
-lammps_b_app="./../lmp -in in.spce"
-lammps_c_app="./../lmp -in in.adp"
-lammps_e_app="./../lmp -in in.rhodo"
+lammps_a_app="./../lmp -in in.spce"
+lammps_b_app="./../lmp -in in.adp"
+lammps_c_app="./../lmp -in in.rhodo"
 cd build/bench
 mpirun -n $2 --hostfile $HOSTFILE $lammps_a_app > $DATASET/$NAME-lammps-A.out 2> $DATASET/$NAME-lammps-A.err
 mpirun -n $2 --hostfile $HOSTFILE $lammps_b_app > $DATASET/$NAME-lammps-B.out 2> $DATASET/$NAME-lammps-B.err
 mpirun -n $2 --hostfile $HOSTFILE $lammps_c_app > $DATASET/$NAME-lammps-C.out 2> $DATASET/$NAME-lammps-C.err
-mpirun -n $2 --hostfile $HOSTFILE $lammps_e_app > $DATASET/$NAME-lammps-e.out 2> $DATASET/$NAME-lammps-e.err
