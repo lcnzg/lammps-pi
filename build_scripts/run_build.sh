35d925e8baea4c806df6306155cc8bf3ec7e6bb9
Luciano Zago <lcnzg@users.noreply.github.com>
2022-06-23 14:38:57 -0300

    Fix PIUtils build

--- build_scripts/run_build.sh
+++ build_scripts/run_build.sh
@@ -1,6 +1,11 @@
 export OMP_NUM_THREADS=2
 cd ..
-mkdir build
+
+cd utils
+make all
+cd ..
+
+mkdir -p build
 cd build
 cmake ../cmake/ -DPKG_KSPACE=on -DPKG_MANYBODY=on -DPKG_RIGID=on -DPKG_MISC=on -DPKG_MOLECULE=on -DBUILD_MPI=on -DPKG_GRANULAR=on -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpic++ -DBUILD_OMP=on
 make -j 2
